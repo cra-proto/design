2feb74bcb0f36674c0f3dd0a3616e745b2b2c0f1
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-07-29 10:31:48 -0600

    Create ucdg.js

---
+++ assets/js/ucdg.js
@@ -0,0 +1,27 @@
+//Loading scripts
+window.onload = menuFooterInject;
+
+
+//Function to add title and menu of guide and footer
+function menuFooterInject() {
+	"use strict";
+		var index = ["menu-ucdg.html", "footer-ucdg.html"];
+		var injectSpot = ["menu-include", "footer-include"];
+var request = new XMLHttpRequest();
+(function loop(i, length) {
+    if (i>= length) {
+        return;
+    }
+    var url = index[i];
+
+    request.open("GET", url, true);
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+    document.getElementById(injectSpot[i]).innerHTML =
+      this.responseText;
+            loop(i + 1, length);
+        }
+    };
+    request.send();
+})(0, index.length);
+}
