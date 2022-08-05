165b6784671f79424070d9f43a949a7374d6bc21
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-08-05 10:58:12 -0600

    Update ucdg.js
    fixed url path

--- assets/js/ucdg.js
+++ assets/js/ucdg.js
@@ -2,9 +2,9 @@
 //Function to add title and menu of guide and footer
 	"use strict";
 		if (document.documentElement.lang === "en") {
-		var index = ["/wet4/development/cxo149/gcweb/nutheme/designs/various/gcmenu-include-en.html", "/wet4/development/cxo149/gcweb/nutheme/designs/various/gcfooter-include-en.html"];
+		var index = ["/design/assets/includes/menu-include-en.html", "/design/assets/includes/footer-include-en.html"];
 }
-if (document.documentElement.lang === "fr") {var index = ["/wet4/development/cxo149/gcweb/nutheme/designs/various/gcmenu-include-fr.html", "/wet4/development/cxo149/gcweb/nutheme/designs/various/gcfooter-include-fr.html"];}
+if (document.documentElement.lang === "fr") {var index = ["/design/assets/includes/menu-include-fr.html", "/design/assets/includes/footer-include-fr.html"];}
 		var injectSpot = ["menu-include", "footer-include"];
 var request = new XMLHttpRequest();
 (function loop(i, length) {
