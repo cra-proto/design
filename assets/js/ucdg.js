d108123e65f72bbc283a80824d0dc3a431ba49d7
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-08-30 16:02:26 -0600

    Update ucdg.js
    added function defining parameter.

--- assets/js/ucdg.js
+++ assets/js/ucdg.js
@@ -25,6 +25,7 @@ var request = new XMLHttpRequest();
 })(0, index.length);
 //function to create inline overlay and to delay button switch to close menu (due to propagation in close event that cannot be properly stopped by JS
 //script also pre-emptively opens details menu by default
+ (function( $, wb ) {
 $(document).on( "wb-ready.wb", function() {
 	$("#mb-pnl .mb-menu details").attr("open", "open");
 	$("#mb-pnl .mb-menu details>ul").attr("aria-hidden", "false");	
@@ -42,4 +43,5 @@ $(document).on( "wb-ready.wb", function() {
 		};
 		console.log("it's closed");
 	});
-});	
+});
+	   })( jQuery, wb );
